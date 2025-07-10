d8d1948702bda8fc8e436f19706b530b7e8310be
yasin-nilco <yasin@nilcoltd.com>
2025-07-10 09:27:40 +0100

    login/register

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -23,6 +24,29 @@ class AuthController extends Controller
     {
         Auth::guard('web')->logout();
         return response()->json(['message' => 'Logged out']);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6',
+            'company'    => 'nullable|string|max:255',
+        ]);
+
+        $user = User::create([
+            'first_name' => $request->first_name,
+            'last_name'  => $request->last_name,
+            'email'      => $request->email,
+            'password'   => Hash::make($request->password),
+            'company'    => $request->company,
+        ]);
+
+        Auth::login($user); // optional
+
+        return response()->json(['message' => 'Registration successful']);
     }
 }
 
